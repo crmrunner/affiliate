e386cda1521a0b2a66f25d282e4c1b52c155fcb1
Satyabrata Biswas <satyabrata.biswas1@mjunction.in>
2022-04-18 21:20:50 +0530

    changed api url

--- src/app/services/app.config.ts
+++ src/app/services/app.config.ts
@@ -1,4 +1,4 @@
 export const APIKey = '332827c90c411790ffa33170c13daa8efb12fd4c';
-export const baseUrl = window.location.host.split(":").indexOf('localhost')>= 0 ? 'http://localhost:4300/' : 'http://employee.crmrunner.com:4600/';
+export const baseUrl = window.location.host.split(":").indexOf('localhost')>= 0 ? 'http://localhost:4300/' : 'https://affiliates.crmrunner.com:4600/';
 export const crmForntEndUrl = "https://crmrunner.com/";
 console.log('hosssssssst: ',window.location.host);
